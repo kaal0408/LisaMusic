add2c9672504fdc437e9a3c43d98378a200b3842
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-12-02 00:06:00 +0530

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -28,7 +28,7 @@ async def play(_, message: Message):
         await app.send_message(message.chat.id,
             text=f"Hello {rpk}!\n\nThis is Electro Private Music Bot.\nI play music on Telegram's Voice Chats.\n\nOnly for selected chats.",
             parse_mode="markdown",
-            reply_markup=pstart_markup,
+            reply_markup=personal_markup,
             reply_to_message_id=message.message_id
         )
     elif len(message.command) == 2:                                                           
