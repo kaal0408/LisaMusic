3e5020563c6bd01d8e94a741bed9c7a669a1e446
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-07 10:03:01 +0530

    Update play.py

--- Music/Plugins/play.py
+++ Music/Plugins/play.py
@@ -56,7 +56,7 @@ def time_to_seconds(time):
         int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":")))
     )
 
-@Client.on_message(command(["play", "play@SkylerX_bot"]))
+@Client.on_message(command(["play", "play@MAHADEVMusicbot"]))
 async def play(_, message: Message):
     chat_id = message.chat.id  
     if message.sender_chat:
