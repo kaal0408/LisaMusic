01827ed8789c68227ad041323538efb779fa7c13
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-05 12:12:26 +0530

    Add files via upload

---
+++ modules/downloaders/youtube.py
@@ -0,0 +1,30 @@
+from os import path
+
+from yt_dlp import YoutubeDL
+
+from modules.config import DURATION_LIMIT
+from modules.helpers.errors import DurationLimitError
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
+        )
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
+        )
+    return path.join("downloads", f"{info['id']}.{info['ext']}")
