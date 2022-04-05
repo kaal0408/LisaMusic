72744de90a0f4bfa239e5151c9615676956af555
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-05 12:16:03 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,17 @@
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+    
+load_dotenv()
+que = {}
+admins = {}
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
+STRING_SESSION = getenv("STRING_SESSION")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
+aiohttpsession = aiohttp.ClientSession()
