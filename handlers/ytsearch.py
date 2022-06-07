491bafb36d69e640b7cfeda9e9780e52b28bac22
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-07 19:12:36 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -4,7 +4,7 @@ import logging
 from pyrogram.types import Message
 from search_engine_parser import GoogleSearch
 from youtube_search import YoutubeSearch
-
+from pyrogram import filters
 from pyrogram import Client as app, filters
 
 logging.basicConfig(
