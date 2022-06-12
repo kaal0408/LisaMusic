2d1eded7fe6660e917083f9254e1a162d9e212fc
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:15:40 +0530

    Update __init__.py

--- Music/database/__init__.py
+++ Music/database/__init__.py
@@ -1,7 +1,7 @@
-## Copyright (©) Team Codexun
+
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from Codexun.config import MONGO_DB_URI
+from Music.config import MONGO_DB_URI
 
 
 MONGODB_CLI = MongoClient(MONGO_DB_URI)
