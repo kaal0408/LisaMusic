648ec5c2ab2d3836c4fe460709bb0629f0c42865
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:15:10 +0530

    Add files via upload

--- Music/database/__init__.py
+++ Music/database/__init__.py
@@ -1,1 +1,8 @@
+## Copyright (©) Team Codexun
 
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from Codexun.config import MONGO_DB_URI
+
+
+MONGODB_CLI = MongoClient(MONGO_DB_URI)
+db = MONGODB_CLI.wbb
