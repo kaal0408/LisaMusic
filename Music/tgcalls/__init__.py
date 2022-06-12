b6fdc6ac7a1c9f3a941f249942142e5bebd4c46e
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:19:39 +0530

    Update __init__.py

--- Music/tgcalls/__init__.py
+++ Music/tgcalls/__init__.py
@@ -1,15 +1,15 @@
-## Copyright (©) Team Codexun
+
 
 from os import listdir, mkdir
 from pyrogram import Client
-from Codexun import config
-from Codexun.tgcalls.queues import clear, get, is_empty, put, task_done
-from Codexun.tgcalls import queues
-from Codexun.tgcalls.youtube import download
-from Codexun.tgcalls.calls import run, pytgcalls
-from Codexun.tgcalls.calls import client
+from Music import config
+from Music.tgcalls.queues import clear, get, is_empty, put, task_done
+from Music.tgcalls import queues
+from Music.tgcalls.youtube import download
+from Music.tgcalls.calls import run, pytgcalls
+from Music.tgcalls.calls import client
 
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from Codexun.tgcalls.convert import convert
+from Music.tgcalls.convert import convert
