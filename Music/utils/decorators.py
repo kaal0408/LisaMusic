7ebde45f904fd4a7f65e07c0a990f0dedeafc5ab
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:22:57 +0530

    Update decorators.py

--- Music/utils/decorators.py
+++ Music/utils/decorators.py
@@ -3,7 +3,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from Codexun.config import SUDO_USERS
+from Music.config import SUDO_USERS
 
 
 def errors(func: Callable) -> Callable:
