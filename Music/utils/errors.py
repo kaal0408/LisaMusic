4a28fc92e010860706dd5455428c8419866632ab
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:24:00 +0530

    Update errors.py

--- Music/utils/errors.py
+++ Music/utils/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram import Client
-from Codexun.config import OWNER_ID
+from Music.config import SUDO_USERS as OWNER_ID
 
 
 def split_limits(text):
