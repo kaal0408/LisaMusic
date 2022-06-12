d5d5df7ec449144e4292b3ead3ec8b5c1114f31b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:20:03 +0530

    Update autoleave.py

--- Music/tgcalls/autoleave.py
+++ Music/tgcalls/autoleave.py
@@ -1,5 +1,5 @@
 from pyrogram.errors import PeerIdInvalid, UserNotParticipant
-from Codexun import pytgcalls, smexy as user
+from Music import pytgcalls, smexy as user
 
 
 async def leave_from_inactive_call():
