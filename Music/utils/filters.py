95adf53447dba87d4032854a868b0125751c0c94
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 15:24:58 +0530

    Update filters.py

--- Music/utils/filters.py
+++ Music/utils/filters.py
@@ -2,7 +2,7 @@ from typing import List, Union
 
 from pyrogram import filters
 
-from Codexun.config import CMD_MUSIC, SUDO_USERS
+from Music.config import CMD_MUSIC, SUDO_USERS
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 
