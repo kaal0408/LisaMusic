ee45729a8492e487f03c326ee76cfd3e6578ac38
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-15 00:47:23 +0530

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -1,4 +1,4 @@
-## Coder are here
+
 
 import os
 from os import getenv
@@ -14,18 +14,16 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 API_ID = int(getenv("API_ID", "8186557"))
 API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-OWNER_NAME = getenv("OWNER_NAME", "Shubhanshu")
+OWNER_NAME = getenv("OWNER_NAME", "Hayat_Murat_30")
 ALIVE_NAME = getenv("ALIVE_NAME", "Null")
-ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "YurikoPlugin")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "Hayat_Murat_30")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "null")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AMANTYA1/RaiChu-MusicV2")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/kaal0408/Music")
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
