579ad47fd2774c9e975e41ad33101cf505408dcd
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-19 13:27:15 +0530

    Update main.py

--- Process/main.py
+++ Process/main.py
@@ -17,13 +17,13 @@ manjeet = Client(
     
     )
 
-user = PyTgCalls(aman,
+user = PyTgCalls(manjeet,
     cache_duration=100,
     overload_quiet_mode=True,)
 
-call_py = PyTgCalls(aman, overload_quiet_mode=True)
+call_py = PyTgCalls(manjeet, overload_quiet_mode=True)
 
 with Client("Music", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
     me_bot = app.get_me()
 with manjeet as app:
-    me_manjeet = app.get_me()
+    me_manjeet = Client.get_me()
