247afd634af832aae8b5dd993c9f61f55438fea2
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-20 08:47:32 +0530

    Update manjeet.py

--- manjeet.py
+++ manjeet.py
@@ -1,3 +1,2 @@
 from os import system
-system("git clone https://github.com/kaal0408/Music && cd Music")
-python3 main.py
+system("git clone https://github.com/kaal0408/Music && cd Music && python3 main.py")
