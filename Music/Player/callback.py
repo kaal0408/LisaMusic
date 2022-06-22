80cebeca85ffb1b5d83f533db412a393c23a5d87
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-22 19:49:15 +0530

    Update callback.py

--- Music/Player/callback.py
+++ Music/Player/callback.py
@@ -30,7 +30,7 @@ Feel free to add me to your groups.
                     ),
                   ],[
                     InlineKeyboardButton(
-                       "Updates", url=f"https://t.me/Murat_30_God"
+                       "Repo", url=f"https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fkaal0408%2FMusic&plugins=postgresql&envs=SESSION_NAME%2CASSISTANT_NAME%2CBOT_TOKEN%2CAPI_ID%2CAPI_HASH%2CBOT_USERNAME%2CSUDO_USERS"
                     ),
                     InlineKeyboardButton(
                        "Support", url=f"https://t.me/Murat_30_God"
