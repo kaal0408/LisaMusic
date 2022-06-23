62960b74ad53c863ff1ed2ffde2e56d7176c1cae
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-23 14:55:31 +0530

    Update Broadcast.py

--- Music/Player/Broadcast.py
+++ Music/Player/Broadcast.py
@@ -7,6 +7,7 @@ from pyrogram.errors import UserAlreadyParticipant
 from Process.main import bot as Ufo
 from Music.config import SUDO_USERS
 
+
 @Client.on_message(filters.command(["gcast"]))
 async def broadcast(_, message: Message):
     sent=0
