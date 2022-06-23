d66268b318ffc6ef65418b256d211ea4a3c24f1a
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-23 14:50:38 +0530

    Update Group.py

--- Music/Player/Group.py
+++ Music/Player/Group.py
@@ -101,7 +101,7 @@ async def start(client: Client, message: Message):
                         " Repo", url=f"https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fkaal0408%2FMusic&plugins=postgresql&envs=SESSION_NAME%2CASSISTANT_NAME%2CBOT_TOKEN%2CAPI_ID%2CAPI_HASH%2CBOT_USERNAME%2CSUDO_USERS"
                     ),
                     InlineKeyboardButton(
-                        "📣 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/Murat_30_God"
+                        "Support Group", url=f"https://t.me/Astro_HelpChat"
                     )
                 ]
             ]
