b2436168eb949cfab02c4c6230a801156f2ffed1
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2023-04-05 21:30:08 +0530

    Update filters.py

--- Lisa/helpers/filters.py
+++ Lisa/helpers/filters.py
@@ -1,6 +1,6 @@
 from typing import Union, List
 from pyrogram import filters
-from modules.config import COMMAND_PREFIXES
+from Lisa.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
